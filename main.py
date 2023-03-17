c99525488c0d8655764ad014f807ce9ba0cb333e
stsav012 <stsav012@gmail.com>
2023-03-17 19:23:20 +0300

    Modern type hints + other refactoring

--- main.py
+++ main.py
@@ -1,45 +1,16 @@
+# coding=utf-8
+from __future__ import annotations
+
 import argparse
-import importlib
 import getpass
 import os
-import sys
 from pathlib import Path
-from typing import List, Type
 from stat import S_ISDIR, S_ISREG
 
-try:
-    from typing import Final
-except ImportError:
-    class _Final:
-        @staticmethod
-        def __getitem__(item: Type):
-            return item
-
-
-    Final = _Final()
-
-paramiko_package: Final[str] = 'paramiko'
-
-try:
-    paramiko = importlib.import_module(paramiko_package)
-except (ImportError, ModuleNotFoundError) as ex:
-    if sys.argv[0] == sys.executable:  # if embedded
-        raise ex
-
-    import subprocess
-
-    if subprocess.check_call([sys.executable, '-m', 'pip', 'install', paramiko_package]):
-        if __name__ == '__main__':
-            print(f'Module `{paramiko_package}` can not be loaded. Make sure it is installed.')
-            sys.exit(1)
-        else:
-            raise ex
-    else:
-        paramiko = importlib.import_module(paramiko_package)
-
+import paramiko
 
 if __name__ == '__main__':
-    def main():
+    def main() -> None:
         ap: argparse.ArgumentParser = argparse.ArgumentParser(
             description='like rsync, update local files with remote ones via SFTP',
             formatter_class=argparse.RawTextHelpFormatter)
@@ -49,11 +20,9 @@ if __name__ == '__main__':
                              'If username is omitted, current local username is used.')
         ap.add_argument('remote_path', help='the path on the remote host to copy files from')
         ap.add_argument('local_path', help='the destination path on the local host')
-        # https://stackoverflow.com/a/4042861/8554611
-        if len(sys.argv) == 1:
-            ap.print_help(sys.stderr)
-            sys.exit(1)
-        args: argparse.Namespace = ap.parse_args()
+        # # if no arguments, print help instead of usage
+        ap.print_usage = ap.print_help
+        args: argparse.Namespace = ap.parse_intermixed_args()
 
         sftp_url: str
         sftp_user: str
@@ -71,7 +40,7 @@ if __name__ == '__main__':
         # automatically add keys without requiring human intervention
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        ssh.connect(sftp_url, username=sftp_user, password=sftp_pass, timeout=1)
+        ssh.connect(sftp_url, username=sftp_user, password=sftp_pass, timeout=1, compress=True)
 
         sftp: paramiko.sftp_client.SFTPClient = ssh.open_sftp()
 
@@ -80,7 +49,7 @@ if __name__ == '__main__':
             local_dir: Path = local_root / remote_path
             local_dir.mkdir(exist_ok=True)
 
-            files: List[paramiko.sftp_attr.SFTPAttributes] = sftp.listdir_attr(str(remote_dir))
+            files: list[paramiko.sftp_attr.SFTPAttributes] = sftp.listdir_attr(str(remote_dir))
             file: paramiko.sftp_attr.SFTPAttributes
 
             def get_file():
@@ -99,12 +68,13 @@ if __name__ == '__main__':
                     if not (local_dir / file.filename).exists():
                         get_file()
                     else:
-                        local_attributes = (local_dir / file.filename).lstat()
+                        local_attributes: os.stat_result = (local_dir / file.filename).lstat()
                         if local_attributes.st_mtime != file.st_mtime:
                             get_file()
                 elif S_ISDIR(file.st_mode):
                     update_dir(remote_path / file.filename)
 
         update_dir()
+
 
     main()
